701ea5d07e39082e2d9ac45fb4b2e6654d2228a4
Marc Brinkmann <git@marcbrinkmann.de>
2011-02-08 21:07:28 +0100

    Cleaned up board test.

--- boardtest.py
+++ boardtest.py
@@ -10,29 +10,22 @@ from math import pi, sqrt
 
 from gamemodel import *
 
-def crossproduct(a, b):
-	return (a[1]*b[2] - a[2]*b[1], a[2]*b[0] - a[0]*b[2], a[0]*b[1] - a[1]*b[0])
-
-
-def dotproduct(a, b):
-	return a[0]*b[0] + a[1]*b[1] + a[2]*b[2]
-
-
 class TileLayout(object):
-	def __init__(self, base, board):
+	def __init__(self, base, board, x_stretch = 3/2., y_stretch = sqrt(3)/2.):
 		self.board = board
+		self.x_stretch = x_stretch
+		self.y_stretch = y_stretch
 
-		sqrt3 = sqrt(3)
 		# we get s == 1 by using to tile-scaling
-		# formulas taken from http://stackoverflow.com/questions/2459402/hexagonal-grid-coordinates-to-pixel-coordinates
-		for (a,b,c), tile in board.tiles.iteritems():
+		# the projection of the tile uses integers, multiplying by
+		# x and y stretch should result in correct coordinates
+		for pos, tile in board.tiles.iteritems():
 			# load model
 			tileModel = base.loader.loadModel(self.get_tile_model_filename(tile))
 
 			# calculate position
-			x = 3/2.*a
-			y = (b-c)*sqrt3/2
-			tileModel.setPos(x, y, 0)
+			x, y = pos.get_projected_coords()
+			tileModel.setPos(x * self.x_stretch, y * self.y_stretch, 0)
 
 			# render
 			tileModel.reparentTo(base.render)
@@ -50,26 +43,17 @@ class MyApp(ShowBase):
 		board.generate_board()
 		print board
 
-		# create 3d model
-#		for pos, tile in b.tiles.iteritems():
-#			print "pos",tile
-
 		TileLayout(self, board)
-
 
 		tileModel = base.loader.loadModel('models/City.egg')
 		tileModel.setPos(0,0,0)
 		tileModel.reparentTo(self.render)
 
+		# ambient light, so we can see colors on models
 		alight = AmbientLight('alight')
 		alight.setColor(VBase4(1, 1, 1, 1))
 		alnp = render.attachNewNode(alight)
 		render.setLight(alnp)
-#		self.camera.setPos(0, 0, 20)
-
-		# control camera
-#		self.camera.setHpr(0, -90, 0)
-#		self.disableMouse()
 
 
 base = MyApp()
