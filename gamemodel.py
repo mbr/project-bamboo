46f44cea5c39ef21910725d7ebe947edba2da3d2
Marc Brinkmann <git@marcbrinkmann.de>
2011-02-09 18:22:36 +0100

    Coastal walking now available as a board method.

--- gamemodel.py
+++ gamemodel.py
@@ -233,6 +233,7 @@ class Board(object):
 		while l > 0:
 			r += 1
 			l -= r*6
+		self.radius = r
 
 		# we will walk from the inside to the outside, so reverse
 		# the chip order
@@ -270,46 +271,6 @@ class Board(object):
 			for i in range(0, len(nodes)):
 				self.network.add_edge(nodes[i], nodes[(i+1)%len(nodes)])
 
-		def outer_edge_walk(r):
-			"""walk along the coast, radius r"""
-			# starting top right tile, top node
-			start_tile = HexPosition(r+1, 0, -r-1)
-			starting_node = tuple(sorted((start_tile, start_tile+HexPosition.directions[-1], start_tile+HexPosition.directions[-2])))
-
-			yield starting_node
-			node_id = starting_node
-			prev_node = node_id
-
-			# try all neighbours
-			candidates = self.network.neighbors(node_id)
-			while candidates:
-				n = candidates.pop()
-
-				# if we visited it just before, skip it
-				if prev_node == n: continue
-
-				# end once we reach the start again
-				if starting_node == n: break
-
-				# must be a node on the coast, i.e. bordering on at least one land (r-1) and sea (r) tile
-				sea, land = False, False
-				for tile_pos in n:
-					distance = tile_pos.distance_to(HexPosition.origin)
-					if distance >= r+1: sea = True
-					else: land = True
-
-				# if it's not a coastal node, skip it
-				if not sea or not land:
-					continue
-
-				# it's a non-visited coastal node - move on to it
-				prev_node = node_id
-				node_id = n
-				yield node_id
-
-				# start with the next set of neighbours
-				candidates = self.network.neighbors(node_id)
-
 		def harbor_placement():
 			# 1, 1, 2, 1, 1, 2, ...
 			order = (True, True, False) * 2 + (True, True, False, False)
@@ -327,8 +288,7 @@ class Board(object):
 		random.shuffle(harbors)
 
 		prev = False
-		for node_id in outer_edge_walk(2):
-			print "walking nodes",node_id
+		for node_id in self.walk_coast():
 			if harbor_place.next():
 				if not prev: harbor = harbors.pop(0)
 				self.network.node[node_id]['harbor'] = harbor
@@ -337,6 +297,47 @@ class Board(object):
 				prev = False
 
 		print "generated network %d nodes, %d edges" % (self.network.number_of_nodes(), self.network.number_of_edges())
+
+	def walk_coast(self):
+		"""walk along the coast, radius r"""
+		# starting top right tile, top node
+		start_tile = HexPosition(self.radius+1, 0, -self.radius-1)
+		starting_node = tuple(sorted((start_tile, start_tile+HexPosition.directions[-1], start_tile+HexPosition.directions[-2])))
+
+		yield starting_node
+		node_id = starting_node
+		prev_node = node_id
+
+		# try all neighbours
+		candidates = self.network.neighbors(node_id)
+		while candidates:
+			n = candidates.pop()
+
+			# if we visited it just before, skip it
+			if prev_node == n: continue
+
+			# end once we reach the start again
+			if starting_node == n: break
+
+			# must be a node on the coast, i.e. bordering on at least one land (r-1) and sea (r) tile
+			sea, land = False, False
+			for tile_pos in n:
+				distance = tile_pos.distance_to(HexPosition.origin)
+				if distance >= self.radius+1: sea = True
+				else: land = True
+
+			# if it's not a coastal node, skip it
+			if not sea or not land:
+				continue
+
+			# it's a non-visited coastal node - move on to it
+			prev_node = node_id
+			node_id = n
+			yield node_id
+
+			# start with the next set of neighbours
+			candidates = self.network.neighbors(node_id)
+
 
 	def node_available(self, node_id):
 		if 'building' in self.network.node[node_id]: return False
